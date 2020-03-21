0d684bd285c0b7e305dd9b091d27e0b24842b2fc
bombren <bombren@tencent.com>
2020-03-21 13:44:14 +0800

    build: init

---
+++ build/precache-manifest.b4b8a938a50748e469d91a21e1352b28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c486aceaa7b2948aec9fce4af38f89c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0b63c2e4e6717a1b06b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "796cbcd724a1432122cd",
+    "url": "/static/js/2.a838b58f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a838b58f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b63c2e4e6717a1b06b",
+    "url": "/static/js/main.55daaa43.chunk.js"
+  },
+  {
+    "revision": "c216085932067799163e",
+    "url": "/static/js/runtime-main.2359674d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
